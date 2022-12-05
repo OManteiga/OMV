ac24677699c6c68218f414a79c85103ee8b7ed67
manteigavazquez <olimanvaz@gmail.com>
2022-12-05 21:41:12 +0100

    PowerShell (Backup Funciones)

--- PowerShell_2(PSDRIVE).ps1
+++ PowerShell_2(PSDRIVE).ps1
@@ -62,3 +62,78 @@ Start-Service "SQLSERVERAGENT"
 
 ### DETENER EL SERVICIO
 Stop-Service "SQLSERVERAGENT"
+
+## PARAR EL SERVICIO DE SQL 
+Set-Service 'MSSQL$DOWSQL2019' -StartupType Disabled
+Stop-Service -Name 'MSSQL$DOWSQL2019' -Force
+
+# PODEMOS EJECUTARLO DESDE UN SCRIPT
+
+PS C:\WINDOWS\system32> & "YOUR PATH\SQLServicesOn.ps1"
+
+powershell.exe -noexit -File “YOUR PATH\SQLServicesOn.ps1”
+
+
+######################
+
+# Flujos de Codigo
+
+$(get-service | where { $_.Name –eq “winrm” })
+
+get-service | ?{$_.Name -eq "Dhcp"}
+
+get-service | ?{$_.Name -eq "wuauserv"}
+get-service | ?{$_.Name -eq "wuauserv"} | %{$_.Start()}
+get-service | ?{$_.Name -eq "wuauserv"}
+get-service | ?{$_.Name -eq "wuauserv"} | %{$_.Stop()}
+Get-Service | Stop-Service -confirm
+
+
+
+
+
+
+##################
+
+############################    DBA TOOLS    ###################################################################
+
+# https://dbatools.io/download/
+
+Install-Module dbatools 
+
+https://dbatools.io/soup2nutz/
+
+
+##esto es para instalarlos solo para el usuario que esta en uso
+Install-Module dbatools -Scope CurrentUser
+
+# verify you have dbatools module installed
+Get-InstalledModule -Name "dbatools"	
+
+# 1.1.143    dbatools       PSGallery       The community module that enables SQL Server Pros to a..
+
+# Quick overview of commands (RESUMEN DE TODOS LOS COMANDOS) 
+Start-Process https://dbatools.io/commands
+
+
+
+################################ BACKUP Y RESTORE CON UN CDMLET ####################################
+
+
+# Almacena los BACKUPS en C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup
+
+Backup-SqlDatabase -ServerInstance "localhost" -Database "Pubs"
+
+
+
+
+# Backup completo con variables y AÑADIENDO LA FECHA
+
+
+$dt = Get-Date -Format yyyyMMddHHmmss
+$instancename = "localhost"
+$dbname = 'Trasteros'
+Backup-SqlDatabase -Serverinstance $instancename -Database $dbname -BackupFile "c:\BACKUP\$($dbname)_db_$($dt).bak"
+
+##Trasteros_db_20221205204130.bak
+
