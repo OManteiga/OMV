d5be3a016b0b2fb2ed083b262ea0e48f61765d24
manteigavazquez <olimanvaz@gmail.com>
2022-12-12 21:37:00 +0100

    Script PowerShell 12/12/22

---
+++ Ejemplo-Segundo.ps1
@@ -0,0 +1,34 @@
+﻿
+#Listing 1: Retrieving Data Through a SqlDataReader Object
+
+
+# Create SqlConnection object and define connection string
+$con = New-Object System.Data.SqlClient.SqlConnection
+$con.ConnectionString = "Server=.; Database=Pubs;   Integrated Security=true"
+$con.open()
+
+# Create SqlCommand object, define command text, and set the connection
+$cmd = New-Object System.Data.SqlClient.SqlCommand
+$cmd.CommandText = "SELECT TOP 10 au_lname, au_fname FROM dbo.authors"
+$cmd.Connection = $con
+
+# Create SqlDataReader
+$dr = $cmd.ExecuteReader()
+
+Write-Host
+
+If ($dr.HasRows)
+{
+  Write-Host Number of fields: $dr.FieldCount
+  Write-Host
+  While ($dr.Read())
+  {
+    Write-Host $dr["au_lname"] $dr["au_fname"]
+  }
+}
+Else
+{
+  Write-Host The DataReader contains no rows.
+}
+
+Write-Host
